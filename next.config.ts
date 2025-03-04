e549990bd195c0b97bc1e75b977ace8838528286
田丰 <tianfeng.1012@bytedance.com>
2025-03-04 16:15:17 +0800

    init

--- next.config.ts
+++ next.config.ts
@@ -1,8 +1,14 @@
+/* next.config.ts */
+// import SemiNext from '@douyinfe/semi-next';
+
 import type { NextConfig } from "next";
+
+// const Semi = SemiNext({})
 
 const nextConfig: NextConfig = {
   /* config options here */
   reactStrictMode: true,
+  transpilePackages: ['@douyinfe/semi-ui', '@douyinfe/semi-icons', '@douyinfe/semi-illustrations'],
 };
 
 export default nextConfig;
